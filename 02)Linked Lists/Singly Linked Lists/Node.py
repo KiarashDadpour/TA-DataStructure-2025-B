8c8a81f0e9689afb0d8a739171ab95b90415dcb0
Kiarash Dadpour <140727992+KiarashDadpour@users.noreply.github.com>
2025-11-02 17:16:47 +0330

    Update Node.py

--- 02)Linked Lists/Singly Linked Lists/Node.py
+++ 02)Linked Lists/Singly Linked Lists/Node.py
@@ -1,1 +1,20 @@
+class Node:
 
+    def __init__(self, info=None, next=None):
+        self.info = info
+        self.next = next
+
+    def get_info(self):
+        return self.info
+
+    def set_info(self, value):
+        self.info = value
+
+    def get_next(self):
+        return self.next
+
+    def set_next(self, ptr):
+        self.next = ptr
+
+    def __str__(self):
+        return str(self.info) 
