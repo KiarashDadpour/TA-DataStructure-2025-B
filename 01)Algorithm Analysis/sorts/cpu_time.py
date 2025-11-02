a412b3dfac064dffc51ceeddc73e1c54a3dbf991
Kiarash Dadpour <140727992+KiarashDadpour@users.noreply.github.com>
2025-11-02 23:34:32 +0330

    Create cpu_time.py

---
+++ 01)Algorithm Analysis/sorts/cpu_time.py
@@ -0,0 +1,12 @@
+import time
+def loop(n):
+    for i in range(n):
+        A = generate_list(50)
+        # sort function
+    return True
+start = time.process_time()
+loop(50_000)
+end = time.process_time()
+
+used_time = end - start
+used_time
